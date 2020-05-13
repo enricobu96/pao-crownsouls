79ca664564bc8140303a007c3bf5a17ce1d41e66
Owyii <50248428+Owyii@users.noreply.github.com>
2020-05-13 12:49:08 +0200

    PepeHands
    Mi sono guardato tutto e ho messo qualcosa su weapon

--- model/core/weapon.h
+++ model/core/weapon.h
@@ -5,7 +5,17 @@
 
 class Weapon : public InventoryItem {
 public:
-    Weapon();
+    Weapon(unsigned int =0, unsigned short =0, unsigned short =0, unsigned short =0,
+           unsigned short =0);
+
+    unsigned int CalculateQualityDmg();
+private:
+    unsigned int BaseDmg;
+    unsigned int QualityDmg;
+    unsigned short StrScaling;
+    unsigned short DexScaling;
+    unsigned short IntScaling;
+    unsigned short FthScaling;
 };
 
 #endif // WEAPON_H
